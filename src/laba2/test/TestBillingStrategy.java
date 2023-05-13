152bd3bad44bfc3aa33860d441274313a810c32c
Ivysaur02 <ig.sochnev@mail.ru>
2023-05-13 12:26:48 +0400

    create Before in test for laba2

--- src/laba2/test/TestBillingStrategy.java
+++ src/laba2/test/TestBillingStrategy.java
@@ -1,38 +1,44 @@
 package laba2.test;
- import laba2.BillingStrategy;
- import laba2.PhoneBilling;
- import laba2.SubscriptionFeeStrategy;
- import laba2.PerMinuteStrategy;
- import laba2.PerSecondStrategy;
- import org.junit.Test;
 
- import static org.junit.Assert.assertEquals;
+import laba2.BillingStrategy;
+import laba2.PhoneBilling;
+import laba2.SubscriptionFeeStrategy;
+import laba2.PerMinuteStrategy;
+import laba2.PerSecondStrategy;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class TestBillingStrategy {
 
-    PhoneBilling phoneBilling = new PhoneBilling(new SubscriptionFeeStrategy(15.00));
+    PhoneBilling phoneBilling;
 
+    @Before
+    public void init() {
+        phoneBilling = new PhoneBilling(new SubscriptionFeeStrategy(15.00));
+    }
 
     @Test
-    public void TestToChangePhoneBillingStrategyToMin(){
+    public void TestToChangePhoneBillingStrategyToMin() {
         phoneBilling.setBillingStrategy(new PerMinuteStrategy(15));
         assertEquals(phoneBilling.toString(), "PerMinuteStrategy");
     }
 
     @Test
-    public void TestToChangePhoneBillingStrategyToSec(){
+    public void TestToChangePhoneBillingStrategyToSec() {
         phoneBilling.setBillingStrategy(new PerSecondStrategy(15));
         assertEquals(phoneBilling.toString(), "PerSecondStrategy");
     }
 
     @Test
-    public void AddCall(){
+    public void AddCall() {
         phoneBilling.addCall(15);
-        assertEquals(phoneBilling.getNumberOfCalls(),1);
+        assertEquals(phoneBilling.getNumberOfCalls(), 1);
     }
 
     @Test
-    public void EvalueteNonChangeStr(){
+    public void EvalueteNonChangeStr() {
         phoneBilling.addCall(15);
         assertEquals(phoneBilling.getTotalDuration(), 15);
     }
